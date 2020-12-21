498483db988d55598808848cff00ad38de89a44a
gpetrov <g.petrov@artsofte.ru>
2020-12-21 17:33:05 +0500

    first look

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(function(){
-  
+    $('.brand').on('click', function(){
+        $(this).parent().find('.brand-product').slideToggle();
+        $(this).parent().find('.brand-list').toggleClass('active');
+    });
+
+    $("#selectAll").on('click', function() {
+        $("input[type=checkbox]").prop("checked", $(this).prop("checked"));
+      });
+      
+      $("input[type=checkbox]").on('click',function() {
+        if (!$(this).prop("checked")) {
+          $("#selectAll").prop("checked", false);
+        }
+      });
 });
