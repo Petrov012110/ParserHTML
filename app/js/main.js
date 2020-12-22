4d10f0ca8f78358a120560a0d8db00de5425fd23
gpetrov <g.petrov@artsofte.ru>
2020-12-22 17:42:23 +0500

    verstka

--- app/js/main.js
+++ app/js/main.js
@@ -2,13 +2,17 @@ $(function(){
     $('.brand').on('click', function(){
         $(this).parent().find('.brand-product').slideToggle();
         $(this).parent().find('.brand-list').toggleClass('active');
+        $(this).parent().find('span').toggleClass('active');
+    });
+    $('.brand-product__li').on('click', function(){
+        $(this).toggleClass('active__li');
     });
 
     $("#selectAll").on('click', function() {
-        $("input[type=checkbox]").prop("checked", $(this).prop("checked"));
+        $(".che").prop("checked", $(this).prop("checked"));
       });
       
-      $("input[type=checkbox]").on('click',function() {
+      $(".che").on('click',function() {
         if (!$(this).prop("checked")) {
           $("#selectAll").prop("checked", false);
         }
