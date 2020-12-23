5af294c582fefb09130fc20651bd4ac3cf4ea6fb
gpetrov <g.petrov@artsofte.ru>
2020-12-23 12:59:16 +0500

    logo

--- app/js/getID.js
+++ app/js/getID.js
@@ -7,12 +7,12 @@ function clickFunction() {
     let elements = document.querySelectorAll('ul>li');
     let newArrId = [];
     
-    for (let elem of elements) {
-        if(elem.className == 'brand-product__li active__li'){
-            newArrId.push(elem.id);
-        }
-    }
-    console.log(newArrId);
+    // for (let elem of elements) {
+    //     if(elem.className == 'brand-product__li active__li'){
+    //         newArrId.push(elem.id);
+    //     }
+    // }
+    console.log(elements);
     return newArrId;
 }
 function clickFunctionCheckbox() {
