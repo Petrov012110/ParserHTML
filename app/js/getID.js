e7e814ea3e000822e5186dad0f68d93af0808ce3
gpetrov <g.petrov@artsofte.ru>
2020-12-23 10:05:33 +0500

    fix getId.js

--- app/js/getID.js
+++ app/js/getID.js
@@ -2,12 +2,14 @@ document.querySelector('.btn').onclick = clickFunction;
 function clickFunction() {
     let elements = document.querySelectorAll('ul>li');
     let newArr = [];
+    
     for (let elem of elements) {
 
         if(elem.className == 'brand-product__li active__li'){
             newArr.push(elem.id);
         }
-        
+
     }
     console.log(newArr);
+    return newArr;
 }
