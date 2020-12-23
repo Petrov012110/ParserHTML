20004b612f0196d7316d6071f076611e0ba7cc03
gpetrov <g.petrov@artsofte.ru>
2020-12-23 11:28:13 +0500

    collet data from front

--- app/js/getID.js
+++ app/js/getID.js
@@ -1,14 +1,26 @@
-document.querySelector('.btn').onclick = clickFunction;
+document.querySelector('.btn').onclick = addDataFrom;
+function addDataFrom(){
+    clickFunction();
+    clickFunctionCheckbox();
+}
 function clickFunction() {
     let elements = document.querySelectorAll('ul>li');
-    let newArr = [];
+    let newArrId = [];
     
     for (let elem of elements) {
-
         if(elem.className == 'brand-product__li active__li'){
-            newArr.push(elem.id);
+            newArrId.push(elem.id);
         }
-
+    }
+    console.log(newArrId);
+    return newArrId;
+}
+function clickFunctionCheckbox() {
+    let checks = document.querySelectorAll('.content-box__filter>label>input:checked');
+    let newArr = [];
+    
+    for (let el of checks) {
+            newArr.push(el.id);
     }
     console.log(newArr);
     return newArr;
